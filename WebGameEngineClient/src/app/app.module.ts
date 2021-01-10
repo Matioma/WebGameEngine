fdb2c9db95e2ef1d02f11e4748d31a3b616c3ea1
Matioma <Artiom.Matievschi@gmail.com>
2021-01-10 20:59:00 +0100

    Add Home Page For Client

--- WebGameEngineClient/src/app/app.module.ts
+++ WebGameEngineClient/src/app/app.module.ts
@@ -2,15 +2,48 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './Components/login/login.component';
+import { HomeComponent } from './Components/home/home.component';
+import { RouterModule } from '@angular/router';
+import { RegisterComponent } from './Components/register/register.component';
+import { HomeNavigationComponent } from './Components/home-navigation/home-navigation.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    HomeComponent,
+    RegisterComponent,
+    HomeNavigationComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      { path: '', redirectTo: 'home', pathMatch: 'full' },
+      { path: 'login', component: LoginComponent },
+      { path: 'register', component: RegisterComponent },
+      {
+        path: 'home',
+        component: HomeComponent,
+        children: [
+          {
+            path: '',
+            component: HomeNavigationComponent,
+          },
+          {
+            path: 'login',
+            component: LoginComponent,
+          },
+          {
+            path: 'register',
+            component: RegisterComponent,
+          },
+        ],
+      },
+    ]),
   ],
+
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
