dbc782f684cfcc0a89451d88ab364f87c3efa045
Matioma <Artiom.Matievschi@gmail.com>
2021-01-11 10:59:28 +0100

    Succesfully read the json file pn the server send from client

--- WebGameEngineServer/server.js
+++ WebGameEngineServer/server.js
@@ -1,13 +1,19 @@
 const express = require("express");
+const bodyparser = require("body-parser");
 
 const app = express();
 const port = 3000;
+
+app.use(bodyparser.json());
 
 app.post("/api/register", (req, res) => {
   console.log("Register Called");
   res.json({ success: "true" });
 });
 app.post("/api/login", (req, res) => {
+  const { login, password } = req.body;
+  console.log(login, password);
+
   console.log("Login Called");
   res.json({ success: "true" });
 });
