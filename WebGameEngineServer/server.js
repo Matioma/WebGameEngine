e5a63e573a1827fb17c82a08f4cb2bc20cb081b6
Matioma <Artiom.Matievschi@gmail.com>
2021-01-11 10:20:05 +0100

    Set up api roots for Login and register

--- WebGameEngineServer/server.js
+++ WebGameEngineServer/server.js
@@ -1,1 +1,17 @@
-console.log("Coolas");
+const express = require("express");
+
+const app = express();
+const port = 3000;
+
+app.post("/api/register", (req, res) => {
+  console.log("Register Called");
+  res.send("Registration Called");
+});
+app.post("/api/login", (req, res) => {
+  console.log("Login Called");
+  res.send("Login Called");
+});
+
+app.listen(port, () => {
+  console.log("Web Game Engine Backend running on port ", port);
+});
