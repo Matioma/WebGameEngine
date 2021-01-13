be7a79e840f79dc4743526eee031c2dc322f2f55
Matioma <Artiom.Matievschi@gmail.com>
2021-01-13 17:52:30 +0100

    css

--- WebGameEngineClient/src/app/Components/project/project.component.ts
+++ WebGameEngineClient/src/app/Components/project/project.component.ts
@@ -3,13 +3,13 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-project',
   templateUrl: './project.component.html',
-  styleUrls: ['./project.component.css']
+  styleUrls: ['./project.component.css'],
 })
 export class ProjectComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
+  Edit() {}
+  Delete() {}
+  Open() {}
 }
